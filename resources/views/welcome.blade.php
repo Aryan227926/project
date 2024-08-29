27a77c1b37f48d193af826711a0a18387ac42d74
Aryan227926 <mraaryaa711@gmail.com>
2024-08-29 20:05:07 +0545

    First test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,12 +85,12 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://laravel.com/docs">HELLO</a>
+                    <a href="https://laracasts.com">Ram</a>
+                    <a href="https://laravel-news.com">Naresh</a>
+                    <a href="https://blog.laravel.com">Das</a>
+                    <a href="https://nova.laravel.com">Abhi</a>
+                    <a href="https://forge.laravel.com">Das</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
